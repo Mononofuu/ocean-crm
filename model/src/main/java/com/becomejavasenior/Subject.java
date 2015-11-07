6e9db611c5ec8cb6fa8939e03e269aa85c41696b
Liya Blieva <liyablieva@gmail.com>
2015-11-07 13:00:28 +0400

    Create Subject.java

---
+++ model/src/main/java/com/becomejavasenior/Subject.java
@@ -0,0 +1,10 @@
+package com.becomejavasenior;
+
+
+import java.io.Serializable;
+
+//Class that represent owner for comments, files and tasks.
+
+public abstract class Subject implements Serializable {
+
+}
