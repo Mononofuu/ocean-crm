e7ef1271c47873bc18de9cb7030e2c275522d143
securelord@gmail.com <1nfnmzyfvfksirf2>
2015-11-28 01:15:01 +0200

    CRMOCEAN-168: refactoring dashboard servlet.

--- controller/src/main/java/com/becomejavasenior/dashboard/DashboardServlet.java
+++ controller/src/main/java/com/becomejavasenior/dashboard/DashboardServlet.java
@@ -9,9 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.Connection;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * @author Lybachevskiy.Vladislav
@@ -22,36 +20,9 @@ public class DashboardServlet extends HttpServlet {
     private static DaoFactory myDaoFactory;
     private static Connection myConnection;
 
-    private static final Map<String, DashboardFunction> SUPPORTED_FUNCTIONS;
-
-    static {
-        SUPPORTED_FUNCTIONS = new HashMap<>();
-        addApiFunction(new DashboardAllDeals());
-        addApiFunction(new DashBoardBudget());
-        addApiFunction(new DashboardDealsWithTasks());
-        addApiFunction(new DashboardDealsWithoutTasks());
-        addApiFunction(new DashboardSuccessDeals());
-        addApiFunction(new DashboardUnsuccessClosedDeals());
-        addApiFunction(new DashboardInprogressTasks());
-        addApiFunction(new DashboardFinishedTasks());
-        addApiFunction(new DashboardOverdueTasks());
-        addApiFunction(new DashboardContacts());
-        addApiFunction(new DashboardCompanies());
-        addApiFunction(new DashboardEvents());
-    }
-
-
     public DashboardServlet() throws DataBaseException {
         myDaoFactory = new PostgreSqlDaoFactory();
         myConnection = myDaoFactory.getConnection();
-    }
-
-    private static void addApiFunction(DashboardFunction apiFunction) {
-        SUPPORTED_FUNCTIONS.put(apiFunction.getName(), apiFunction);
-    }
-
-    protected Map<String, DashboardFunction> getSupportedFunction() {
-        return SUPPORTED_FUNCTIONS;
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -63,34 +34,128 @@ public class DashboardServlet extends HttpServlet {
     }
 
     private void processServlet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        for (DashboardFunction command : getSupportedFunction().values()) {
-            command.execute(request);
-        }
+        request.setAttribute("allDeals", String.valueOf(getAllDeals().size()));
+        request.setAttribute("dealsBudget", getDealsBudget());
+        request.setAttribute("dealWithTasks", getDealWithTasks());
+        request.setAttribute("dealsWithoutTasks", getDealsWithoutTasks());
+        request.setAttribute("successDeals", getSuccessDeals());
+        request.setAttribute("unsuccessClosedDeals", getUnsuccessClosedDeals());
+        request.setAttribute("tasksInProgress", tasksInProgress());
+        request.setAttribute("finishedTasks", getTaskProcessingWithParameters("Завершено"));
+        request.setAttribute("overdueTasks", getTaskProcessingWithParameters("Не завершено"));
+        request.setAttribute("contacts", getContacts());
+        request.setAttribute("companies", getCompanies());
+        request.setAttribute("events", getEvents());
         getServletContext().getRequestDispatcher("/dashboard.jsp").forward(request, response);
     }
 
-    public interface DashboardFunction {
-
-        String getName();
-
-        void execute(HttpServletRequest request) throws IOException;
+    private int getEvents() {
+        GenericDao eventsDao = getGenericDao(Event.class);
+        List events = null;
+        try {
+            events = eventsDao.readAll();
+        } catch (DataBaseException e) {
+            e.printStackTrace();
+        }
+        return events != null ? events.size() : 0;
     }
 
-    private static class DashboardAllDeals implements DashboardFunction {
-
-        @Override
-        public String getName() {
-            return "getAllDeals";
+    private int getCompanies() {
+        GenericDao companiesDao = getGenericDao(Company.class);
+        List companies = null;
+        try {
+            companies = companiesDao.readAll();
+        } catch (DataBaseException e) {
+            e.printStackTrace();
         }
-
-        @Override
-        public void execute(HttpServletRequest request) throws IOException {
-            List deals = getAllDeals();
-            request.setAttribute(getName(), String.valueOf(deals.size()));
-        }
+        return companies != null ? companies.size() : 0;
     }
 
-    private static GenericDao getGenericDao(Class daoClass) {
+    private int getContacts() {
+        GenericDao contactsDao = getGenericDao(Contact.class);
+        List contacts = null;
+        try {
+            contacts = contactsDao.readAll();
+        } catch (DataBaseException e) {
+            e.printStackTrace();
+        }
+        return contacts != null ? contacts.size() : 0;
+    }
+
+    private int tasksInProgress() {
+        GenericDao tasksDao = getGenericDao(Task.class);
+        List tasks = null;
+        try {
+            tasks = tasksDao.readAll();
+        } catch (DataBaseException e) {
+            e.printStackTrace();
+        }
+        return tasks != null ? tasks.size() : 0;
+    }
+
+    private int getUnsuccessClosedDeals() {
+        List deals = getAllDeals();
+        int unsuccessClosedDeals = 0;
+        for (Object deal : deals) {
+            if (deal instanceof Deal) {
+                if (((Deal) deal).getStatus().getName().equals("Закрыто и нереализовано")) {
+                    unsuccessClosedDeals++;
+                }
+            }
+        }
+        return unsuccessClosedDeals;
+    }
+
+    private int getSuccessDeals() {
+        List deals = getAllDeals();
+        int successDeals = 0;
+        for (Object deal : deals) {
+            if (deal instanceof Deal) {
+                if (((Deal) deal).getStatus().getName().equals("Успешно реализовано")) {
+                    successDeals++;
+                }
+            }
+        }
+        return successDeals;
+    }
+
+    private int getDealsWithoutTasks() {
+        List deals = getAllDeals();
+        int dealsWithoutTasks = 0;
+        for (Object deal : deals) {
+            if (deal instanceof Deal) {
+                if (((Deal) deal).getTasks().size() == 0) {
+                    dealsWithoutTasks++;
+                }
+            }
+        }
+        return dealsWithoutTasks;
+    }
+
+    private double getDealsBudget() {
+        List deals = getAllDeals();
+        double dealsBudget = 0;
+        assert deals != null;
+        for (Object deal : deals) {
+            if (deal instanceof Deal) {
+                dealsBudget = dealsBudget + ((Deal) deal).getBudget();
+            }
+        }
+        return dealsBudget;
+    }
+
+    private int getDealWithTasks() {
+        List deals = getAllDeals();
+        int dealsWithTasks = 0;
+        for (Object deal : deals) {
+            if (deal instanceof Deal) {
+                dealsWithTasks = dealsWithTasks + ((Deal) deal).getTasks().size();
+            }
+        }
+        return dealsWithTasks;
+    }
+
+    private GenericDao getGenericDao(Class daoClass) {
         GenericDao dealGenericDao = null;
         try {
             dealGenericDao = myDaoFactory.getDao(myConnection, daoClass);
@@ -100,46 +165,7 @@ public class DashboardServlet extends HttpServlet {
         return dealGenericDao;
     }
 
-    private static class DashBoardBudget implements DashboardFunction {
-        @Override
-        public String getName() {
-            return "getDealsBudget";
-        }
-
-        @Override
-        public void execute(HttpServletRequest request) throws IOException {
-            List deals = getAllDeals();
-            double dealsBudget = 0;
-            assert deals != null;
-            for (Object deal : deals) {
-                if (deal instanceof Deal) {
-                    dealsBudget = dealsBudget + ((Deal) deal).getBudget();
-                }
-            }
-            request.setAttribute(getName(), dealsBudget);
-        }
-    }
-
-    private static class DashboardDealsWithTasks implements DashboardFunction {
-        @Override
-        public String getName() {
-            return "getDealWithTasks";
-        }
-
-        @Override
-        public void execute(HttpServletRequest request) throws IOException {
-            List deals = getAllDeals();
-            int dealsWithTasks = 0;
-            for (Object deal : deals) {
-                if (deal instanceof Deal) {
-                    dealsWithTasks = dealsWithTasks + ((Deal) deal).getTasks().size();
-                }
-            }
-            request.setAttribute(getName(), dealsWithTasks);
-        }
-    }
-
-    private static List getAllDeals() {
+    private List getAllDeals() {
         GenericDao genericDao = getGenericDao(Deal.class);
         List deals = null;
         try {
@@ -150,116 +176,7 @@ public class DashboardServlet extends HttpServlet {
         return deals;
     }
 
-    private static class DashboardDealsWithoutTasks implements DashboardFunction {
-        @Override
-        public String getName() {
-            return "getDealsWithoutTasks";
-        }
-
-        @Override
-        public void execute(HttpServletRequest request) throws IOException {
-            List deals = getAllDeals();
-            int dealsWithoutTasks = 0;
-            for (Object deal : deals) {
-                if (deal instanceof Deal) {
-                    if (((Deal) deal).getTasks().size() == 0) {
-                        dealsWithoutTasks++;
-                    }
-                }
-            }
-            request.setAttribute(getName(), dealsWithoutTasks);
-        }
-    }
-
-    private static class DashboardSuccessDeals implements DashboardFunction {
-        @Override
-        public String getName() {
-            return "successDeals";
-        }
-
-        @Override
-        public void execute(HttpServletRequest request) throws IOException {
-            List deals = getAllDeals();
-            int successDeals = 0;
-            for (Object deal : deals) {
-                if (deal instanceof Deal) {
-                    if (((Deal) deal).getStatus().getName().equals("Успешно реализовано")) {
-                        successDeals++;
-                    }
-                }
-            }
-            request.setAttribute(getName(), successDeals);
-        }
-    }
-
-    private static class DashboardUnsuccessClosedDeals implements DashboardFunction {
-        @Override
-        public String getName() {
-            return "unsuccessClosedDeals";
-        }
-
-        @Override
-        public void execute(HttpServletRequest request) throws IOException {
-            List deals = getAllDeals();
-            int unsuccessClosedDeals = 0;
-            for (Object deal : deals) {
-                if (deal instanceof Deal) {
-                    if (((Deal) deal).getStatus().getName().equals("Закрыто и нереализовано")) {
-                        unsuccessClosedDeals++;
-                    }
-                }
-            }
-            request.setAttribute(getName(), unsuccessClosedDeals);
-        }
-    }
-
-    private static class DashboardInprogressTasks implements DashboardFunction {
-        @Override
-        public String getName() {
-            return "tasksInProgress";
-        }
-
-        @Override
-        public void execute(HttpServletRequest request) throws IOException {
-            GenericDao tasksDao = getGenericDao(Task.class);
-            List tasks = null;
-            try {
-                tasks = tasksDao.readAll();
-            } catch (DataBaseException e) {
-                e.printStackTrace();
-            }
-            assert tasks != null;
-            request.setAttribute(getName(), tasks.size());
-        }
-    }
-
-    private static class DashboardFinishedTasks implements DashboardFunction {
-        @Override
-        public String getName() {
-            return "finishedTasks";
-        }
-
-        @Override
-        public void execute(HttpServletRequest request) throws IOException {
-            taskProcessingWithParameters(request, getName(), "Завершено");
-        }
-    }
-
-    private static class DashboardOverdueTasks implements DashboardFunction {
-        @Override
-        public String getName() {
-            return "overdueTasks";
-        }
-
-        @Override
-        public void execute(HttpServletRequest request) throws IOException {
-            taskProcessingWithParameters(request, getName(), "Не завершено");
-        }
-
-
-    }
-
-    private static void taskProcessingWithParameters(HttpServletRequest request, String name, String status) {
+    private int getTaskProcessingWithParameters(String status) {
         GenericDao tasksDao = getGenericDao(Task.class);
         List tasks = null;
         try {
@@ -276,65 +193,7 @@ public class DashboardServlet extends HttpServlet {
                 }
             }
         }
-        request.setAttribute(name, processingTasksCount);
+        return processingTasksCount;
     }
 
-    private static class DashboardContacts implements DashboardFunction {
-        @Override
-        public String getName() {
-            return "contacts";
-        }
-
-        @Override
-        public void execute(HttpServletRequest request) throws IOException {
-            GenericDao contactsDao = getGenericDao(Contact.class);
-            List contacts = null;
-            try {
-                contacts = contactsDao.readAll();
-            } catch (DataBaseException e) {
-                e.printStackTrace();
-            }
-            assert contacts != null;
-            request.setAttribute(getName(), contacts.size());
-        }
-    }
-
-    private static class DashboardCompanies implements DashboardFunction {
-        @Override
-        public String getName() {
-            return "companies";
-        }
-
-        @Override
-        public void execute(HttpServletRequest request) throws IOException {
-            GenericDao companiesDao = getGenericDao(Company.class);
-            List companies = null;
-            try {
-                companies = companiesDao.readAll();
-            } catch (DataBaseException e) {
-                e.printStackTrace();
-            }
-            assert companies != null;
-            request.setAttribute(getName(), companies.size());
-        }
-    }
-
-    private static class DashboardEvents implements DashboardFunction {
-        @Override
-        public String getName() {
-            return "events";
-        }
-
-        @Override
-        public void execute(HttpServletRequest request) throws IOException {
-            GenericDao eventsDao = getGenericDao(Event.class);
-            List events = null;
-            try {
-                events = eventsDao.readAll();
-            } catch (DataBaseException e) {
-                e.printStackTrace();
-            }
-            request.setAttribute(getName(), events);
-        }
-    }
 }
