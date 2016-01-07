4b583c1d7d1ddce422a9173a9fada7d42adfe0f0
anton.sakhno <sakhno83@gmail.com>
2016-01-07 20:53:58 +0200

    dao fix in task service

--- service/src/main/java/com/becomejavasenior/impl/TaskServiceImpl.java
+++ service/src/main/java/com/becomejavasenior/impl/TaskServiceImpl.java
@@ -16,35 +16,41 @@ import java.util.Map;
  */
 public class TaskServiceImpl implements TaskService {
     private Logger logger = LogManager.getLogger(TaskServiceImpl.class);
-    private final TaskDAOImpl myDao = new TaskDAOImpl();
+    private DaoFactory dao;
+    private TaskDAOImpl taskDao;
+
+    public TaskServiceImpl() throws DataBaseException {
+        dao = new PostgreSqlDaoFactory();
+        taskDao = (TaskDAOImpl)dao.getDao(Task.class);
+    }
 
     @Override
     public void saveTask(Task task) throws DataBaseException {
         if (task.getId() == 0) {
-            myDao.create(task);
+            taskDao.create(task);
         } else {
-            myDao.update(task);
+            taskDao.update(task);
         }
     }
 
     @Override
     public void deleteTask(int id) throws DataBaseException {
-        myDao.delete(id);
+        taskDao.delete(id);
     }
 
     @Override
     public Task findTaskById(int id) throws DataBaseException {
-        return myDao.read(id);
+        return taskDao.read(id);
     }
 
     @Override
     public List<Task> getAllTask() throws DataBaseException {
-        return myDao.readAll();
+        return taskDao.readAll();
     }
 
     @Override
     public List<Task> getTasksBySubject(Subject subject) throws DataBaseException {
-        return myDao.getAllTasksBySubjectId(subject.getId());
+        return taskDao.getAllTasksBySubjectId(subject.getId());
     }
 
     @Override
@@ -89,6 +95,6 @@ public class TaskServiceImpl implements TaskService {
         if (!"".equals(user)){
             userId = user;
         }
-        return myDao.getAllTasksByParameters(userId, date, taskTypeId);
+        return taskDao.getAllTasksByParameters(userId, date, taskTypeId);
     }
 }
