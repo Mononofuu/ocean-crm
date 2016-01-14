12d9192a6adc89d8d3354d8a0fe257e91e3ce4fa
anton.sakhno <sakhno83@gmail.com>
2016-01-14 18:59:31 +0200

    dao changed to service layer in ViewAllContactsServlet

--- controller/src/main/java/com/becomejavasenior/ViewAllContactsServlet.java
+++ controller/src/main/java/com/becomejavasenior/ViewAllContactsServlet.java
@@ -1,5 +1,9 @@
 package com.becomejavasenior;
 
+import com.becomejavasenior.impl.CompanyServiceImpl;
+import com.becomejavasenior.impl.ContactServiceImpl;
+import com.becomejavasenior.impl.TagServiceImpl;
+import com.becomejavasenior.impl.UserServiceImpl;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -18,7 +22,6 @@ import java.util.List;
 @WebServlet(name = "contactlist", urlPatterns = "/contactlist")
 public class ViewAllContactsServlet extends HttpServlet {
     private Logger logger = LogManager.getLogger(TaskListServlet.class);
-    private DaoFactory dao;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -32,21 +35,20 @@ public class ViewAllContactsServlet extends HttpServlet {
 
     private void process(HttpServletRequest req, HttpServletResponse resp) throws IOException{
         try {
-            dao = new PostgreSqlDaoFactory();
-            GenericDao contactDao = dao.getDao(Contact.class);
-            List<Contact> allContacts = contactDao.readAll();
+            ContactService contactService = new ContactServiceImpl();
+            CompanyService companyService = new CompanyServiceImpl();
+            UserService userService = new UserServiceImpl();
+            TagService tagService = new TagServiceImpl();
+            List<Contact> allContacts = contactService.findContacts();
             Collections.sort(allContacts, (o1, o2) -> o1.getName().compareTo(o2.getName()));
             req.setAttribute("contactlist", allContacts);
-            GenericDao companyDao = dao.getDao(Company.class);
-            List<Company> allCompanies = companyDao.readAll();
+            List<Company> allCompanies = companyService.findCompanies();
             Collections.sort(allCompanies, ((o1, o2) -> o1.getName().compareTo(o2.getName())));
             req.setAttribute("companylist", allCompanies);
-            GenericDao tagsDao = dao.getDao(Tag.class);
-            List<Tag> tags = tagsDao.readAll();
+            List<Tag> tags = tagService.getAllTags();
             Collections.sort(tags, ((o1, o2) -> o1.getName().compareTo(o2.getName())));
             req.setAttribute("tags", tags);
-            GenericDao<TaskType> usersDao = dao.getDao(User.class);
-            req.setAttribute("users", usersDao.readAll());
+            req.setAttribute("users", userService.getAllUsers());
             getServletContext().getRequestDispatcher("/jsp/viewallcontacts.jsp").forward(req,resp);
         } catch (DataBaseException e) {
             logger.error("Error when prepearing data for tasklist.jsp",e);
---
+++ service/src/main/java/com/becomejavasenior/impl/TagServiceImpl.java
@@ -0,0 +1,51 @@
+package com.becomejavasenior.impl;
+
+import com.becomejavasenior.*;
+import com.becomejavasenior.interfacedao.TagDAO;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.List;
+
+/**
+ * @author Anton Sakhno <sakhno83@gmail.com>
+ */
+public class TagServiceImpl implements TagService {
+    private static Logger logger = LogManager.getLogger(TagServiceImpl.class);
+    private DaoFactory dao;
+    private TagDAO tagDao;
+
+    public TagServiceImpl() {
+        try {
+            dao = new PostgreSqlDaoFactory();
+            tagDao = (TagDAOImpl)dao.getDao(Tag.class);
+        } catch (DataBaseException e) {
+            logger.error(e);
+        }
+
+    }
+
+    @Override
+    public void saveTag(Tag tag) throws DataBaseException {
+        if (tag.getId() == 0) {
+            tagDao.create(tag);
+        } else {
+            tagDao.update(tag);
+        }
+    }
+
+    @Override
+    public void deleteTag(int id) throws DataBaseException {
+        tagDao.delete(id);
+    }
+
+    @Override
+    public Tag findTagById(int id) throws DataBaseException {
+        return tagDao.read(id);
+    }
+
+    @Override
+    public List<Tag> getAllTags() throws DataBaseException {
+        return tagDao.readAll();
+    }
+}
