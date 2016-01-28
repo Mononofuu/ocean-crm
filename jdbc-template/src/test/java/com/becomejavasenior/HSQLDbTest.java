15097d761a86f800742fd67a3202e8413e106538
Mononofuu <mononofuu@gmail.com>
2016-01-28 13:17:57 +0200

    Sql creation and data init scripts for in-mem DB added

--- jdbc-template/src/test/java/com/becomejavasenior/HSQLDbTest.java
+++ jdbc-template/src/test/java/com/becomejavasenior/HSQLDbTest.java
@@ -17,15 +17,12 @@ import java.sql.Statement;
  * created by Alekseichenko Sergey <mononofuu@gmail.com>
  */
 
-//@Sql(scripts = "data_hsql.sql")
 @RunWith(SpringJUnit4ClassRunner.class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @ContextConfiguration(locations = {"classpath:spring-datasource.xml"})
 public class HSQLDbTest {
-    //    @Qualifier("dataSource")
     @Autowired
     DataSource dataSource;
-
 
     @Test
     public void selectAllTest() {
@@ -34,7 +31,6 @@ public class HSQLDbTest {
             connection = dataSource.getConnection();
             Statement statement = connection.createStatement();
             ResultSet rs = statement.executeQuery("SELECT * FROM CURRENCY");
-            System.out.println(rs.getFetchSize());
             while (rs.next()) {
                 System.out.println(rs.getInt(1));
                 System.out.println(rs.getString(2));
