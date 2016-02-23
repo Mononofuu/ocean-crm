d4ee03ae65c44608d91ab33fb6728d2241c46d25
Mononofuu <mononofuu@gmail.com>
2016-02-23 18:58:07 +0200

    Task REST

---
+++ rest/src/main/java/com/becomejavasenior/controllers/TaskController.java
@@ -0,0 +1,42 @@
+package com.becomejavasenior.controllers;
+
+import com.becomejavasenior.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * created by Alekseichenko Sergey <mononofuu@gmail.com>
+ */
+@RestController
+@RequestMapping("rest/tasks")
+public class TaskController {
+    @Autowired
+    TaskService taskService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Task> getTasks() throws DataBaseException {
+        return taskService.getAllTask();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Task getTask(@PathVariable int id) throws DataBaseException {
+        return taskService.findTaskById(id);
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.PUT)
+    public void addTask(@RequestBody Task task) throws DataBaseException {
+        taskService.saveTask(task);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.POST)
+    public void editTask(@PathVariable int id, @RequestBody Task task) throws DataBaseException {
+        taskService.saveTask(task);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void deleteTask(@PathVariable int id) throws DataBaseException {
+        taskService.deleteTask(id);
+    }
+}
